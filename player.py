7c67f764eaf00be2fc46289c99d12d95607588ae
limitedink <54423958+limitedink@users.noreply.github.com>
2025-09-06 13:18:49 +1000

    feat: add shooting cooldown timer

--- player.py
+++ player.py
@@ -6,6 +6,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -35,6 +36,7 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
+        self.timer -= dt
 
     def move(self,dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -42,7 +44,13 @@ class Player(CircleShape):
 
     def shoot(self):
         from circleshape import Shot
+        if self.timer > 0:
+            return
+
         shot = Shot(self.position.x, self.position.y)
         velocity = pygame.Vector2(0, 1).rotate(self.rotation)
         shot.velocity = velocity * PLAYER_SHOOT_SPEED
+        cooldown = 0.3 
+        self.timer = cooldown 
+        
         
